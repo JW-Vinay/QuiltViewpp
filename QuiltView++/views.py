2a3bcd61ef05462b3948cccd561c4176cf38f387
JW-Vinay <vinay.ramkrishnan@gmail.com>
2014-11-15 21:05:54 +0000

    fixed bugs related to timestamp and quit

--- QuiltView++/views.py
+++ QuiltView++/views.py
@@ -33,7 +33,6 @@ app.config['MYSQL_DATABASE_DB'] = 'quiltview'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-
 # launch flask configurations
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
@@ -51,7 +50,8 @@ def showHome():
 def submitQuery():
 	if request.method == 'POST':
 		query = request.form['query']
-		print query
+		isRefresh = request.form['refresh_status']
+		print query, isRefresh
 		if (query is not None) and (query != ''):
 	    		cursor = mysql.connect().cursor()
 	    		queryString = "SELECT a.query_id, a.query_item, b.stream_id, b.is_stream_active, b.rtmp_url FROM queries a INNER JOIN live_streams b ON a.query_id = b.query_id WHERE is_stream_active=true and a.query_item='%s';" % (query)
@@ -60,22 +60,28 @@ def submitQuery():
 	    			#queryString  = "SELECT * FROM queries WHERE query_item='%s';" %(query)
 	    			#cursor.execute(queryString)
 	    			#if cursor.fetchone() is None:
-	    			
-		 		#insert to query table check value of no_subscribers update only play event is received from nginx
-		 		queryString = "INSERT into queries(query_item, no_active_streamers, no_subscribers) VALUES('%s','%s', %s);" % (query, 0,0)
-		 		try:
-		    			cursor.execute(queryString)
-		    		 	cursor.connection.commit()
-		    		 	data = 	{"message":"Waiting for response.", "status":200, "count" : 0}
-					resp = json.dumps(data)
-					#return resp						
-				except IntegrityError as e:
-					data = 	{"message":"Duplicate entry. Please try again.", "status":-1, "count" : 0}
-					resp = 	json.dumps(data)
-					#return resp
-			#else:
-			#	data = 	data = 	{"message":"Duplicate entry. Please try again", "status":-1, "count" : 0}
-			#		resp = json.dumps(data)		
+	    			if (isRefresh is not None) and isRefresh == "true":
+					print isRefresh
+					data = {"message":"Duplicate entry. Please try again.", "status":-1, "count" : 0}
+				else:
+					#insert to query table check value of no_subscribers update only play event is received from nginx
+			 		queryString = "INSERT into queries(query_item, no_active_streamers, no_subscribers) VALUES('%s','%s', %s);" % (query, 0,0)
+			 		try:
+			    			cursor.execute(queryString)
+		    			 	cursor.connection.commit()
+		    			 	data = 	{"message":"Waiting for response.", "status":200, "count" : 0}
+						resp = json.dumps(data)
+																
+					except IntegrityError as e:
+						#data = 	{"message":"Duplicate entry. Please try again.", "status":-1, "count" : 0}
+						#resp = 	json.dumps(data)
+						queryString  = "UPDATE queries SET timestamp  = now()  WHERE query_item = '%s';" % (query)
+						cursor.execute(queryString)
+						cursor.connection.commit()
+						data =  {"message":"Waiting for response.", "status":200, "count" : 0}
+					
+				resp = json.dumps(data)
+				print resp
 				return resp	
 	    		else:
 	    			# commenting subscriber count update if query is already present in the database
@@ -200,14 +206,14 @@ def getNoSubscribers():
 	if request.method == 'POST':
 		streamName = request.form['name']
 		callType = request.form['call']
-		#print "name ", streamName
+		print "name action", callType
 		streamUrl = "rtmp://%s:1935/mytv/%s" % (STREAM_SERVER, streamName)
 		streamId = "SELECT query_id FROM live_streams WHERE rtmp_url='%s'" %(streamUrl)
 		#print "stream id ", streamId
 		if callType == "play":
-			queryString  = "UPDATE queries SET no_subscribers = no_subscribers +1 WHERE query_id= (%s);" %(streamId)
+			queryString  = "UPDATE queries SET no_subscribers = no_subscribers +1, timestamp = timestamp  WHERE query_id= (%s);" %(streamId)
 		else:
-			queryString  = "UPDATE queries SET no_subscribers = no_subscribers -1 WHERE query_id= (%s);" %(streamId)
+			queryString  = "UPDATE queries SET no_subscribers = no_subscribers -1, timestamp = timestamp WHERE query_id= (%s) and no_subscribers > 0;" %(streamId)
 		#print "query  ", queryString
 		cursor = mysql.connect().cursor()
 		cursor.execute(queryString)	
@@ -239,32 +245,36 @@ def quitStream():
 		queryId = request.form['query_id']
 		streamId = request.form['stream_id']
 		if queryId is not None and streamId is not None:
-			resp = Response(response=None, status=204, mimetype=None)
 			cursor = mysql.connect().cursor()
 			query = "SELECT pid FROM live_streams WHERE stream_id=%s" % (streamId)
 			cursor.execute(query)
-			pid = cursor.fetchone()[0]
-			print "pid", pid
-			killFFMPEG(pid)	
-			query = "UPDATE queries SET no_active_streamers = no_active_streamers-1 WHERE query_id = '" + queryId + "';"
-			cursor.execute(query)	
-			cursor.connection.commit()
-			query  = "SELECT no_active_streamers FROM queries WHERE query_id = %s;" % (queryId)
-			cursor.execute(query)
-			streamersCount = cursor.fetchone()
-			if streamersCount is not None:
-				 count = streamersCount[0]
-				 if count <= 0:
-				 	# set stream active bit to 0 when no of streamers is 0 and set no_subscribers to 0 for this case as well
-				 	query = "UPDATE live_streams SET is_stream_active = 0 WHERE stream_id = '" + streamId + "';"
-				 	cursor.execute(query)	
-					cursor.connection.commit()
-					query  = "UPDATE queries SET no_subscribers = 0 WHERE query_id = %s;" % (queryId)
-					cursor.execute(query)	
-					cursor.connection.commit()	
-			return resp 
+			cursorTuples = cursor.fetchone()
+			if cursorTuples is not None:
+				pid = cursorTuples[0]
+				#print "pid", pid
+				killFFMPEG(pid)	
+				query = "UPDATE queries SET no_active_streamers = no_active_streamers-1, timestamp = timestamp WHERE query_id = %s and no_active_streamers > 0;" % (queryId)
+				cursor.execute(query)	
+				cursor.connection.commit()
+				query  = "SELECT no_active_streamers FROM queries WHERE query_id = %s;" % (queryId)
+				cursor.execute(query)
+				streamersCount = cursor.fetchone()
+				resp = Response(response=None, status=204, mimetype=None)
+				if streamersCount is not None:
+					 count = streamersCount[0]
+					 if count <= 0:
+					 	# set stream active bit to 0 when no of streamers is 0 and set no_subscribers to 0 for this case as well
+					 	query = "UPDATE live_streams SET is_stream_active = 0 WHERE stream_id = %s;" % (streamId)
+					 	cursor.execute(query)	
+						cursor.connection.commit()
+						query  = "UPDATE queries SET no_subscribers = 0, timestamp = timestamp  WHERE query_id = %s and no_subscribers > 0;" % (queryId)
+						cursor.execute(query)	
+						cursor.connection.commit()	
+				return resp 
+			else:
+				return errorResponse("Invalid ID", 404) 
 		else:	
-			return errorResponse("Invalid ID", 200)
+			return errorResponse("Invalid ID", 404)
 
 		
 @app.route('/post-reply', methods = ['POST'])
@@ -301,7 +311,7 @@ def updateLiveStreamers(ip, queryId):
 	print rtsp_url
 	print rtmp_url
 	cursor = mysql.connect().cursor()
-	query = "UPDATE queries SET no_active_streamers = no_active_streamers+1 Where query_id = '" + queryId + "';"
+	query = "UPDATE queries SET no_active_streamers = no_active_streamers+1, timestamp = timestamp Where query_id = '" + queryId + "';"
 	cursor.execute(query)	
 	cursor.connection.commit()
 	if cursor.rowcount == 1: 
@@ -327,7 +337,8 @@ def getQueries():
 		return val
 		
 	cursor = mysql.connect().cursor()
-	cursor.execute("SELECT * from queries where TIME_TO_SEC(timediff(now(),timestamp))<=%s;" %(20))
+	#print "SELECT * from queries where TIME_TO_SEC(timediff(now(),timestamp))<=%s ORDER BY timestamp DESC;"
+	cursor.execute("SELECT * from queries where TIME_TO_SEC(timediff(now(),timestamp))<=%s ORDER BY timestamp DESC;" %(20))
 	queryList = generateJsonForQueries(cursor)    	
 	if queryList:
 		#resp = Response(json.dumps(queryList), status=200, mimetype='application/json')
@@ -376,22 +387,26 @@ def stream(stream_id, rtmp_url=None, rtsp_url=None):
 	#ip = "128.237.209.240:1234"
 	#call_command("avconv -i %s -ar 44100 -f flv %s" % (rtsp_url, rtmp_url))
 	#call_command("avconv -i rtsp://%s -ar 44100 -r 30 -vsync cfr -q 30 -fflags nobuffer -f flv rtmp://%s:1935/mytv/vinay-phone" % (ip, STREAM_SERVER))
-	cursor = mysql.connect().cursor()
-	queryString  = "SELECT rtmp_url from live_streams WHERE stream_id=%d" % (stream_id)
-	cursor.execute(queryString)
-	data  = cursor.fetchone();
-	print data
-	if data is not None:
-		rtmp_url = data[0]
-		streamName = rtmp_url[rtmp_url.rfind("/")+1:]
-		rtmp_url = rtmp_url[0:rtmp_url.rfind("/")+1]
-		print "stream name", streamName
-		print "rtmp ",rtmp_url
-		#return render_template('index.html', stream_name="vinay-phone", stream_url="rtmp://%s:1935/mytv/" % (STREAM_SERVER))
-		#return render_template('index.html', stream_name=streamName, stream_url=rtmp_url)
-		return render_template('jwplayback.html', stream_name=streamName, stream_url=rtmp_url)
-	else:
-		return redirect(url_for('showHome')) 
+	if 'logged_in' in session and session.get('logged_in'):
+		queryString  = "SELECT rtmp_url from live_streams WHERE stream_id=%d" % (stream_id)
+		cursor = mysql.connect().cursor()
+		cursor.execute(queryString)
+		data  = cursor.fetchone();
+		#print data
+		if data is not None:
+			rtmp_url = data[0]
+			streamName = rtmp_url[rtmp_url.rfind("/")+1:]
+			rtmp_url = rtmp_url[0:rtmp_url.rfind("/")+1]
+			#print "stream name", streamName
+			#print "rtmp ",rtmp_url
+			#return render_template('index.html', stream_name="vinay-phone", stream_url="rtmp://%s:1935/mytv/" % (STREAM_SERVER))
+			#return render_template('index.html', stream_name=streamName, stream_url=rtmp_url)
+			return render_template('jwplayback.html', stream_name=streamName, stream_url=rtmp_url)
+		else:
+			return redirect(url_for('showHome')) 
+
+        else:
+                return render_template('login.html')
 
 @app.route('/logout')
 def logout():
